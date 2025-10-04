f5e12f70b63dd29d25f784e4e874a7ef023c02c9
Ajay Kumar <ajaykumarmkaju@gmail.com>
2025-10-04 16:20:52 +0530

    add: implement TrustedBy component and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import TrustedBy from "./components/TrustedBy";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -11,6 +12,7 @@ const App = () => {
     <div className="dark:bg-black realtive">
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
     </div>
   );
 };
