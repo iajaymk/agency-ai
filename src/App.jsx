14b92e950ca59be7d8a9f13c0e699fc8ed2610fc
Ajay Kumar <ajaykumarmkaju@gmail.com>
2025-10-09 12:38:34 +0530

    add: implement custom cursor functionality and update CSS to hide default cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
@@ -14,6 +14,43 @@ const App = () => {
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
 
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
+
+  // reference for custom cursor position tracking
+  const mouse = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      mouse.current.x = event.clientX;
+      mouse.current.y = event.clientY;
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.2;
+      position.current.y += (mouse.current.y - position.current.y) * 0.2;
+
+      if (dotRef.current && outlineRef.current) {
+        dotRef.current.style.transform = `translate3d(${
+          mouse.current.x - 6
+        }px, ${mouse.current.y - 6}px, 0)`;
+        outlineRef.current.style.transform = `translate3d(${
+          position.current.x - 15
+        }px, ${position.current.y - 15}px, 0)`;
+      }
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <div className="dark:bg-black realtive">
       <Toaster />
@@ -25,6 +62,18 @@ const App = () => {
       <Teams />
       <ContactUs />
       <Footer theme={theme} />
+
+      {/* Custom Cursor Ring */}
+      <div
+        ref={outlineRef}
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+        style={{ transition: "transform 0.1s ease-out" }}
+      ></div>
+      {/* Custom cursor dot */}
+      <div
+        className="fixed top-0 left-o h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"
+        ref={dotRef}
+      ></div>
     </div>
   );
 };
